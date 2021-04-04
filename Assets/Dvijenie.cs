30e72d843b03f89822f7754fa973710d34fa1ee9
Gleb333-3 <glebefremenko23@gmail.com>
2021-04-04 22:47:37 +0300

    commit

---
+++ Assets/Dvijenie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dvijenie : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _direction;
+    [SerializeField]
+    private float _speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _direction.x = Input.GetAxis("Horizontal");
+        _direction.z = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        var mov = _direction * (_speed * Time.deltaTime);
+        transform.Translate(mov);
+    }
+}
---
+++ Assets/image.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class image : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _image;
+    void Start()
+    {
+        _image.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _image.SetActive(true);
+        }
+    }
+}
