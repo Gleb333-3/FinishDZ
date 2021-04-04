48fd23f80845bed653fb8985d1a2056602b170f9
Gleb333-3 <glebefremenko23@gmail.com>
2021-04-04 22:49:09 +0300

    commit

---
+++ Assets/Sciptotmena.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sciptotmena : MonoBehaviour
+{
+    private AudioSource _AudioSource;
+
+    private void Awake()
+    {
+        _AudioSource = GetComponent<AudioSource>();
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _AudioSource.Stop();
+        }
+    }
+}
---
+++ Assets/Scrimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrimer : MonoBehaviour
+{
+    private AudioSource _AudioSource;
+
+    private void Awake()
+    {
+        _AudioSource = GetComponent<AudioSource>();
+
+    }
+
+    private void Start()
+    {
+        _AudioSource.Stop();
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _AudioSource.Play();
+        }
+    }
+}
